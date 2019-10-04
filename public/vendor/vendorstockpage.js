95afc8c02c39a33e92a9bba50fd05b53c08114eb
Saurabh Gupta <saurabhgupta1699@gmail.com>
2019-10-04 20:42:23 +0530

    update stock page done
    Signed-off-by: Saurabh Gupta <saurabhgupta1699@gmail.com>

--- public/vendor/vendorstockpage.js
+++ public/vendor/vendorstockpage.js
@@ -2,28 +2,41 @@ $(function(){
     $.get('/vendor/getallproducts',(data)=>{
         printingdetails(data);
     })
-    $('#sel').change(function(e){
-       // let r=$('#sel').val();
-       console.log($(this));
-        console.log($(this).children("option:selected").val());
-        console.log(r);
-        $.get('/vendor/getstock',(data)=>{
-            printstock(data);
-        })
+    $('#next').click(()=>{
+        console.log('next clicked') 
+        // $('.inp').attr('disabled')
+        var selected_option =$( "#sel option:selected" ).text(); 
+        var stock=$('#num').val()
+        var price=$('#price').val()
+        var desc=$('#desc').val()  
+    $.post('/vendor/updatestock',{stock:stock,price:price,description:desc,productname:selected_option},function(data){
+        console.log(data)
+    })    
     })
-    
+ 
   });
 
 function printingdetails(data){
     products=$('#products')
-    sel=$(`<select ${name="list"} ${id="sel"}></select>`)
+    sel=$(`<select></select>`)
     for(let i=0;i<data.length;i++){
         sel.append(`<option value="${data[i].name}">${data[i].name}</option>`)
 
 
     }
+    $(sel).attr('id','sel');
+    sel.change(()=>{
+        console.log('njfsdkjfdsdkj')
+        var selected_option =$( "#sel option:selected" ).text();
+        console.log(selected_option)
+        $.post('/vendor/stockstatus',{name:selected_option},function(data){
+            console.log(data)
+            $('#num').val(data.stock)
+            $('#price').val(data.price)
+            $('#desc').val(data.description)
+        })
+    })
     products.append(sel);
-
 }
 
 
